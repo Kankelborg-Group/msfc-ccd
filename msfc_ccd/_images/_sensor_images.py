a80ef7d844b28ebaaa7ec6980bd127a0cf331384
Roy Smart <roytsmart@gmail.com>
2024-03-25 21:44:04 -0600

    Allowed some parameters of `msfc_ccd.SensorData` to take default values.

--- msfc_ccd/_images/_sensor_images.py
+++ msfc_ccd/_images/_sensor_images.py
@@ -46,35 +46,35 @@ class SensorData(
     timedelta_requested: u.Quantity | na.AbstractScalar = dataclasses.MISSING
     """The requested exposure time of each image."""
 
-    serial_number: str | na.AbstractScalar = dataclasses.MISSING
+    serial_number: None | str | na.AbstractScalar = None
     """The serial number of the camera that captured each image."""
 
-    run_mode: str | na.AbstractScalar = dataclasses.MISSING
+    run_mode: None | str | na.AbstractScalar = None
     """The Run Mode of the camera when each image was captured."""
 
-    status: str | na.AbstractScalar = dataclasses.MISSING
+    status: None | str | na.AbstractScalar = None
     """The status of the camera while each image was being captured."""
 
-    voltage_fpga_vccint: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    voltage_fpga_vccint: u.Quantity | na.AbstractScalar = 0
     """The VCCINT voltage of the FPGA when each image was captured."""
 
-    voltage_fpga_vccaux: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    voltage_fpga_vccaux: u.Quantity | na.AbstractScalar = 0
     """The VCCAUX voltage of the FPGA when each image was captured."""
 
-    voltage_fpga_vccbram: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    voltage_fpga_vccbram: u.Quantity | na.AbstractScalar = 0
     """The VCCBRAM voltage of the FPGA when each image was captured."""
 
-    temperature_fpga: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    temperature_fpga: u.Quantity | na.AbstractScalar = 0
     """The temperature of the FPGA when each image was captured."""
 
-    temperature_adc_1: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    temperature_adc_1: u.Quantity | na.AbstractScalar = 0
     """Temperature 1 of the ADC when each image was captured."""
 
-    temperature_adc_2: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    temperature_adc_2: u.Quantity | na.AbstractScalar = 0
     """Temperature 2 of the ADC when each image was captured."""
 
-    temperature_adc_3: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    temperature_adc_3: u.Quantity | na.AbstractScalar = 0
     """Temperature 3 of the ADC when each image was captured."""
 
-    temperature_adc_4: u.Quantity | na.AbstractScalar = dataclasses.MISSING
+    temperature_adc_4: u.Quantity | na.AbstractScalar = 0
     """Temperature 4 of the ADC when each image was captured."""
