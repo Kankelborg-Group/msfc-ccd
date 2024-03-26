3e3a507ba1e846873da9225ff2decf575682e59b
Roy Smart <roytsmart@gmail.com>
2024-03-25 21:24:10 -0600

    Fixing some Ruff F401 errors.

--- msfc_ccd/_images/__init__.py
+++ msfc_ccd/_images/__init__.py
@@ -2,4 +2,8 @@
 A package for representing data gathered from imaging sensors
 """
 
+__all__ = [
+    "SensorData",
+]
+
 from ._sensor_images import SensorData
--- msfc_ccd/abc.py
+++ msfc_ccd/abc.py
@@ -2,4 +2,9 @@
 Abstract base classes used throughout this library.
 """
 
+__all__ = [
+    "AbstractImageData",
+    "AbstractSensorData",
+]
+
 from ._images.abc import AbstractImageData, AbstractSensorData
