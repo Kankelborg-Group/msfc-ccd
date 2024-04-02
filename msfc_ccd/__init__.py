5507cad61ec3f8ff24c3bb7f30401217c0fc035c
Roy Smart <roytsmart@gmail.com>
2024-04-01 20:04:35 -0600

    Added `msfc_ccd.fits.open()` convenience function.

--- msfc_ccd/__init__.py
+++ msfc_ccd/__init__.py
@@ -5,9 +5,11 @@ A Python package for the CCD cameras developed by MSFC.
 __all__ = [
     "samples",
     "SensorData",
+    "fits",
     "abc",
 ]
 
 from . import samples
 from ._images import SensorData
+from . import fits
 from . import abc
