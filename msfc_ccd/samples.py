7f4770522cf9818ca9edeb1ae282fb823b6f52e0
Roy Smart <roytsmart@gmail.com>
2025-07-18 14:23:20 -0600

    Added dark sample images from the 2019 ESIS flight. (#10)

--- msfc_ccd/samples.py
+++ msfc_ccd/samples.py
@@ -7,6 +7,8 @@ import pathlib
 __all__ = [
     "path_fe55_esis1",
     "path_fe55_esis3",
+    "path_dark_esis1",
+    "path_dark_esis3",
 ]
 
 path_fe55_esis1 = pathlib.Path(__file__).parent / "_data/fe55/ESIS1_00002.fit"
@@ -19,4 +21,16 @@ path_fe55_esis3 = pathlib.Path(__file__).parent / "_data/fe55/ESIS3_05384.fit"
 """
 An Fe 55 sample image from the ESIS channel 3 camera gathered by the MSFC 
 sounding rocket team during camera testing and validation on 2017-07-12. 
+"""
+
+path_dark_esis1 = pathlib.Path(__file__).parent / "_data/darks/ESIS1_00099.fit.gz"
+"""
+A sample dark image from the ESIS channel 1 camera captured during the ESIS launch
+on 2019-09-30
+"""
+
+path_dark_esis3 = pathlib.Path(__file__).parent / "_data/darks/ESIS3_00099.fit.gz"
+"""
+A sample dark image from the ESIS channel 3 camera captured during the ESIS launch
+on 2019-09-30
 """
