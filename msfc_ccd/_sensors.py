b3efa40633d7923b64028d394b0d1594025a5790
Roy Smart <roytsmart@gmail.com>
2025-08-14 15:41:54 -0600

    Renamed `width_package` and `height_package` to `width_package_x` and `width_package_y`. (#33)

--- msfc_ccd/_sensors.py
+++ msfc_ccd/_sensors.py
@@ -147,10 +147,10 @@ class TeledyneCCD230(
     temperature: u.Quantity | na.AbstractScalar = 248 * u.K
     """The operating temperature of this sensor."""
 
-    width_package: u.Quantity = 42 * u.mm
+    width_package_x: u.Quantity = 42 * u.mm
     """The horizontal size of the physical sensor package."""
 
-    height_package: u.Quantity = 61 * u.mm
+    width_package_y: u.Quantity = 61 * u.mm
     """The vertical size of the physical sensor package."""
 
     @property
